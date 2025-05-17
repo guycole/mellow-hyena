2dd4f73c97c4600797696ee49222c41fc87b5424
Guy Cole <guycole@gmail.com>
2025-05-16 19:53:15 -0700

    fix destination directory

--- bin/wombat04-to-s3.sh
+++ bin/wombat04-to-s3.sh
@@ -10,7 +10,7 @@ PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 TODAY=$(date '+%Y-%m-%d')
 FILE_NAME="hyena-${TODAY}.tgz"
 #
-DEST_BUCKET=s3://mellow-hyena-uw2-t8833.braingang.net/archive/
+DEST_BUCKET=s3://mellow-hyena-uw2-t8833.braingang.net/fresh/
 #
 SOURCE_DIR="archive"
 WORK_DIR="/var/mellow/hyena/fresh"
